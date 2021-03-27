f15768fd2f3167f10cf5e696dbda01e476a1a860
RRbell <rrbell1027@gmail.com>
2021-03-27 17:58:52 +0800

    mainActivity新增ListView

--- app/src/main/java/com/example/n5tango/MainActivity.java
+++ app/src/main/java/com/example/n5tango/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.n5tango;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.LauncherActivity;
 import android.content.Intent;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListAdapter;
 
 public class MainActivity extends AppCompatActivity {
+    String ListItemName[] = {"新增單字集", "查看單字集", "練習"};
 
     Intent intent;
 
@@ -62,6 +67,68 @@ public class MainActivity extends AppCompatActivity {
         question100 = (Button)findViewById(R.id.question100);
         chinese = (Button)findViewById(R.id.chineseQuiz);
         hannji = (Button)findViewById(R.id.hannjiQuiz);
+
+        ListAdapter adapter = new ListAdapter() {
+            @Override
+            public boolean areAllItemsEnabled() {
+                return true;
+            }
+
+            @Override
+            public boolean isEnabled(int position) {
+                return false;
+            }
+
+            @Override
+            public void registerDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public void unregisterDataSetObserver(DataSetObserver observer) {
+
+            }
+
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public boolean hasStableIds() {
+                return false;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                return null;
+            }
+
+            @Override
+            public int getItemViewType(int position) {
+                return 0;
+            }
+
+            @Override
+            public int getViewTypeCount() {
+                return 0;
+            }
+
+            @Override
+            public boolean isEmpty() {
+                return false;
+            }
+        };
 
         intent = new Intent(MainActivity.this, Question.class);
     }
