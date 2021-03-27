93c9e4d1cc44fdd2d81b7ef80f2d3a6cd0863211
RRbell <rrbell1027@gmail.com>
2021-03-27 20:10:08 +0800

    list增加內容

--- app/src/main/java/com/example/n5tango/MainActivity.java
+++ app/src/main/java/com/example/n5tango/MainActivity.java
@@ -8,15 +8,48 @@ import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-    String ListItemName[] = {"新增單字集", "查看單字集", "練習"};
+    String ListItemName[] = new String[]{"新增單字集", "查看單字集", "練習"};
 
     Intent intent;
 
     public Button question10, question50, question100, chinese, hannji;
+    public ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        question10 = (Button) findViewById(R.id.question10);
+        question50 = (Button) findViewById(R.id.question50);
+        question100 = (Button) findViewById(R.id.question100);
+        chinese = (Button) findViewById(R.id.chineseQuiz);
+        hannji = (Button) findViewById(R.id.hannjiQuiz);
+        list = (ListView) findViewById(R.id.list);
+
+        intent = new Intent(MainActivity.this, Question.class);
+
+        setAdapter();
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
+
+    void setAdapter() {
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ListItemName);
+        list.setAdapter(adapter);
+    }
 
     public void ChooseNumber(View v) {
         int value = 0;
@@ -32,14 +65,14 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
 
-        intent.putExtra("Amount",value);
+        intent.putExtra("Amount", value);
         startActivity(intent);
         chinese.setVisibility(View.VISIBLE);
         hannji.setVisibility(View.VISIBLE);
         question10.setVisibility(View.GONE);
         question50.setVisibility(View.GONE);
         question100.setVisibility(View.GONE);
-        }
+    }
 
     public void ChooseType(View v) {
         switch (v.getId()) {
@@ -55,81 +88,5 @@ public class MainActivity extends AppCompatActivity {
         question10.setVisibility(View.VISIBLE);
         question50.setVisibility(View.VISIBLE);
         question100.setVisibility(View.VISIBLE);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        question10 = (Button)findViewById(R.id.question10);
-        question50 = (Button)findViewById(R.id.question50);
-        question100 = (Button)findViewById(R.id.question100);
-        chinese = (Button)findViewById(R.id.chineseQuiz);
-        hannji = (Button)findViewById(R.id.hannjiQuiz);
-
-        ListAdapter adapter = new ListAdapter() {
-            @Override
-            public boolean areAllItemsEnabled() {
-                return true;
-            }
-
-            @Override
-            public boolean isEnabled(int position) {
-                return false;
-            }
-
-            @Override
-            public void registerDataSetObserver(DataSetObserver observer) {
-
-            }
-
-            @Override
-            public void unregisterDataSetObserver(DataSetObserver observer) {
-
-            }
-
-            @Override
-            public int getCount() {
-                return 0;
-            }
-
-            @Override
-            public Object getItem(int position) {
-                return null;
-            }
-
-            @Override
-            public long getItemId(int position) {
-                return 0;
-            }
-
-            @Override
-            public boolean hasStableIds() {
-                return false;
-            }
-
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                return null;
-            }
-
-            @Override
-            public int getItemViewType(int position) {
-                return 0;
-            }
-
-            @Override
-            public int getViewTypeCount() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-        };
-
-        intent = new Intent(MainActivity.this, Question.class);
     }
 }
